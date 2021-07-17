60047cbbd56af998d59b22341174cf448b052f67
Altay02 <Busurmankulov.altay@mail.ru>
2021-07-17 14:48:28 +0300

    фронт дизайн

--- Seki/seki_tester.py
+++ Seki/seki_tester.py
@@ -1,30 +1,4 @@
-from direct_solver import SekiSolver
-from numpy import array
-import sys
 
-height, width = map(int, input("height, width:").split())
-
-raw = []
-for i in range(height):
-  arr = [*map(int, input().split())]
-  if len(arr) != width:
-    print("Row too long!", file=sys.stderr)
-    sys.exit(1)
-  raw.append(arr)
-
-field = array(raw, dtype=int)
-solver = SekiSolver(field, type="seki")
-
-while True:
-  print(solver.field.data)
-  p = (*map(int, input("x, y").split()),)
-  solver.decrement(*p)
-  pr = solver.find_optimal(False)
-  if pr is None:
-    print("No optimal responce for C")
-    break
-  print(pr.value, pr.x, pr.y)
-  solver.decrement(pr.x, pr.y)
 from direct_solver import SekiSolver
 from numpy import array
 import sys

b4858c9c9ce60a226e16fb77499963fbadd4e5a8
Max <maksloboda123@gmail.com>
2021-08-11 11:41:00 +0300

    Added permutation generation

--- Seki/python/generate_field.py
+++ Seki/python/generate_field.py
@@ -1,6 +1,7 @@
 import numpy as np
 import random
 import os
+import sys
 
 def array_of_sum(cnt, s):
     indexes = np.arange(cnt)
@@ -16,6 +17,22 @@ def generate_field(sum, n, m):
 
     return seki_field
 
+def generate_permutation(size):
+    eye = np.eye(size)
+    rng = np.random.default_rng()
+    return rng.permutation(eye, axis=1)
+
+def generate_complex_permutations(size, max_allowed_element, sum):
+    if size * max_allowed_element < sum:
+        print("Impossible sum requested!", file=sys.stderr)
+        return
+    current = generate_permutation(size)
+    while np.sum(current[0, :]) < sum:
+        new = current + generate_permutation(size)
+        if np.max(new) <= max_allowed_element:
+            current = new
+    return current
+
 def setup_dir(path):
     if not path.endswith("_sf"):
         raise Exception(
@@ -26,20 +43,26 @@ def setup_dir(path):
         if os.path.isfile(full_path):
             os.unlink(full_path)
 
-
 def generate_samples(number, sum, n, m):
     output_directory = os.getenv("MATRIX_DIR", "test_matricies_sf")
     setup_dir(output_directory)
-    for i in range(number):
-        with open(os.path.join(output_directory, str(i)), "w") as f:
-            seki_field = generate_field(sum, n, m)
-            f.write("{0} {1}\n".format(n, m))
-            for i in range(m):
-                for j in range(n):
-                    f.write("{0} ".format(seki_field[i, j]))
-                f.write("\n")
+    for n in range(2, 7):
+        for k in range(2, min(n * 2, int(20 / n) + 1)):
+            for c in range(100):
+                print(n, k, c)
+                file_name = "{n}_{k}_{c}".format(n=n, k=k, c=c)
+                with open(os.path.join(output_directory, file_name), "w") as f:
+                    #print(f)
+                    seki_field = generate_complex_permutations(n, 2, k).astype(int)
+                    f.write("{0} {1}\n".format(n, n))
+                    for i in range(n):
+                        for j in range(n):
+                            f.write("{0} ".format(seki_field[i, j]))
+                        f.write("\n")
 
 if __name__ == "__main__":
+    #print(generate_complex_permutations(4, 2, 6))
+    #exit()
     num = int(input())
     sum = int(input())
     n = int(input())
